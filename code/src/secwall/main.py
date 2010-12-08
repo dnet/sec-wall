f32bd5a873cef7f1f3f3061dd7e0a331e01471b4
Dariusz Suchojad <dsuch@gefira.pl>
2010-12-08 22:10:42 +0100

    Refactored the secwall.main's code further out. Added tests for secwall.main.

--- code/src/secwall/main.py
+++ code/src/secwall/main.py
@@ -38,14 +38,17 @@ description = 'sec-wall {0}- A feature packed high-performance security proxy'.f
 init_help = 'Initializes a config directory'
 start_help = 'Starts sec-wall in the given directory'
 stop_help = 'Stops a sec-wall instance running in the given directory'
-fork_help = "Starts one of the sec-wall's subprocesses"
+fork_help = 'Starts one of the sec-wall\'s subprocesses'
 
 class MyFormatter(argparse.ArgumentDefaultsHelpFormatter):
     """ A nicer help formatter, setting 'max_help_position' to that value
     ensures the help doesn't span multiple lines in an awkward way.
     """
+    max_help_position = 35
+
     def __init__(self, **kwargs):
-        super(MyFormatter, self).__init__(max_help_position=35, **kwargs)
+        super(MyFormatter, self).__init__(max_help_position=self.max_help_position,
+                                          **kwargs)
 
     def _get_help_string(self, action):
         """ Overridden from the super-class to prevent showing of defaults,
@@ -54,6 +57,9 @@ class MyFormatter(argparse.ArgumentDefaultsHelpFormatter):
         return action.help
 
 def get_parser():
+    """ Returns an argparse.ArgumentParser instance for parsing the command line
+    arguments.
+    """
     parser = argparse.ArgumentParser(prog='sec-wall', description=description,
                                      formatter_class=MyFormatter)
     group = parser.add_mutually_exclusive_group(required=True)
--- code/tests/test_main.py
+++ code/tests/test_main.py
@@ -19,11 +19,80 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+# stdlib
+import copy, uuid
+
 # nose
 from nose.tools import assert_true, eq_
 
 # sec-wall
-from secwall import main
+from secwall import core, main
 
-def test_main():
-    pass
+def test_global():
+    """ Checks global constants.
+    """
+    eq_(main.description, 'sec-wall {0}- A feature packed high-performance security proxy'.format(core.version))
+    eq_(main.init_help, 'Initializes a config directory')
+    eq_(main.start_help, 'Starts sec-wall in the given directory')
+    eq_(main.stop_help, 'Stops a sec-wall instance running in the given directory')
+    eq_(main.fork_help, 'Starts one of the sec-wall\'s subprocesses')
+
+def test_help_formatter():
+    """ Checks whether our custom help formatter works as expected.
+    """
+    eq_(main.MyFormatter.max_help_position, 35)
+
+    formatter = main.MyFormatter(prog='ignored')
+    help_value = uuid.uuid4().hex
+
+    class _MyAction(object):
+        help = help_value
+
+    # When given an action, the formatter should simply return the value
+    # of its 'help' attribute without any tinkering about it.
+    help_string = formatter._get_help_string(_MyAction())
+    eq_(help_string, help_value)
+
+def test_parser():
+    """ Tests whether the command line parser has expected attributes.
+    """
+    parser = main.get_parser()
+    eq_(parser.prog, 'sec-wall')
+    eq_(parser.description, main.description)
+    eq_(parser.formatter_class, main.MyFormatter)
+    eq_(len(parser._actions), 5)
+
+    # A set of actions expected to be defined by the parser. Each name will
+    # be popped off the 'expected_actions' set in a loop below. It is an error
+    # if anything is left in the 'expected_actions' set when the loop finishes.
+    base_expected_actions = set(['help', 'init', 'start', 'stop', 'fork'])
+    expected_actions = copy.deepcopy(base_expected_actions)
+
+    expected = {
+        'help': dict(option_strings=['-h', '--help'], help='show this help message and exit'),
+        'init': dict(option_strings=[u'--init'], help=main.init_help),
+        'start': dict(option_strings=[u'--start'], help=main.start_help),
+        'stop': dict(option_strings=[u'--stop'], help=main.stop_help),
+        'fork': dict(option_strings=[u'--fork'], nargs=2, help=main.fork_help, metavar=(u'config_dir', u'is_https'))
+    }
+
+    for action in parser._actions:
+        assert_true(action.dest in base_expected_actions,
+                    (action.dest, base_expected_actions))
+
+        expected_option_strings = expected[action.dest]['option_strings']
+        expected_help = expected[action.dest]['help']
+
+        eq_(expected_option_strings, action.option_strings)
+        eq_(expected_help, action.help)
+
+        if action.dest == 'fork':
+            expected_nargs = expected[action.dest]['nargs']
+            expected_metavar = expected[action.dest]['metavar']
+
+            eq_(expected_nargs, action.nargs)
+            eq_(expected_metavar, action.metavar)
+
+        expected_actions.remove(action.dest)
+
+    eq_(len(expected_actions), 0, expected_actions)
